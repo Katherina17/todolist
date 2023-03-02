807e21ff9da3ff8fe3138e8e2169fff164d962e3
katherina17 <katherinepetrova76@gmail.com>
2023-03-02 22:25:01 +0300

    feat: start to work with the server

---
+++ 1-todolist/src/api/todolist-api.ts
@@ -0,0 +1,57 @@
+import axios from "axios";
+
+const instance = axios.create({
+        baseURL: 'https://social-network.samuraijs.com/api/1.1/',
+        withCredentials: true,
+        /*headers: {
+            // Не забываем заменить API-KEY на собственный
+            'API-KEY': '43a60f90-0234-478e-9be6-fcfce5403846',
+        },*/
+    })
+
+
+export const todolistAPI = {
+    getTodoList(){
+        return instance.get<TodoListType[]>('todo-lists')
+    },
+    createTodoList(title:string){
+        return instance.post<ResponseType<{item: TodoListType}>>('todo-lists',{title})
+    },
+    deleteTodolist(todoListID: string){
+        return instance.delete<ResponseType>(`todo-lists/${todoListID}`)
+    },
+    updateTodolistTitle(todoListID: string, title: string){
+        return instance.put<ResponseType>(`todo-lists/${todoListID}`, {title})
+    }
+
+}
+
+type TodoListType = {
+    addedDate: string,
+    id: string,
+    order: number,
+    title: string
+}
+
+type ResponseType<T = {}> = {
+    fieldsErrors: string[]
+    messages: string[]
+    resultCode: number
+    data: T
+}
+
+type updateTodolistTitle = {
+    data: {}
+    fieldsErrors: string[]
+    messages: string[]
+    resultCode: number
+}
+
+
+type createTodolist = {
+    data: {items: TodoListType}
+    fieldsErrors: string[]
+    messages: string[]
+    resultCode: number
+}
+
